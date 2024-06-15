4f37e1ab3c840560d9f9422146446c299b74b1e7
AzerOuerghi <azer.werghi@gmail.com>
2024-06-15 19:57:19 +0100

    chore: Update code formatting and add missing newlines

--- immoluxe-ui/src/app/app-routing.module.ts
+++ immoluxe-ui/src/app/app-routing.module.ts
@@ -3,7 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { authGuard } from './services/guard/auth.guard';
 
+import { PropertyListComponent } from './properties-list/properties-list.component';
+import { AddPropertyComponent } from './add-properties/add-properties.component';
+import { UpdatePropertyComponent } from './update-properties/update-properties.component';
+import { ShowDetailsComponent } from './show-details/show-details.component';
+
+  
+
 const routes: Routes = [
+  {path:"show-all-properties",component: PropertyListComponent},
+  {path:"add-properties", component: AddPropertyComponent},
+  {path:'updating-by-id/:id',component:UpdatePropertyComponent},
+  {path:'details-of-properties/:id',component:ShowDetailsComponent},
+  {path:'home',component:HomeComponent},
   {
     path: '',
     component: HomeComponent,
--- immoluxe-ui/src/app/pages/home/home.component.ts
+++ immoluxe-ui/src/app/pages/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+
+
+import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  standalone: true,
+	imports: [NgbCarouselModule]
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class HomeComponent {
+  
+  constructor(
+   
+    private router: Router) { 
+      
+    }
+  goToEmployeeList(){
+    this.router.navigate(['/show-all-properties']);
   }
+  // images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/2000/600`);
 
 }
