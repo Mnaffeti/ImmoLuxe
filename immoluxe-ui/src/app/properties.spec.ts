b14ad266e8c5e0d87bf2c2f7f5659d351039f566
zaoui <zaouieya2@gmail.com>
2024-06-25 15:43:45 +0100

    update

--- immoluxe-ui/src/app/properties.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Property } from './properties';
-
-describe('Property', () => {
-  it('should create an instance', () => {
-    expect(new Property()).toBeTruthy();
-  });
-});
--- server/src/main/java/com/example/immoluxe/Entity/StatusProperty.java
+++
@@ -1,6 +0,0 @@
-package com.example.immoluxe.Entity;
-
-public enum StatusProperty
-{
-    Disponible , EnCoursDeVente , Vendue , EnLocation ;
-}
