51788ded1a3f1f2d86e631269132d5b14232a92d
zaoui <zaouieya2@gmail.com>
2024-06-25 23:54:28 +0100

    update

---
+++ ImmoLuxe/immoluxe-ui/src/app/contrats.ts
@@ -0,0 +1,44 @@
+export class Contrat {
+  id !: number;
+ // idAgent!: number;
+//  idClient!: number;
+  //idProprietaire!: number;
+  //idProperty!: Property;
+  dateDebut!: Date;
+  dateFin!: Date;
+  typeContrat !: TypeContrat;
+  montant!: number ;
+  createdBy! :String  ;
+  createdDate !: Date;
+  /*Ajouter */
+  proprietaire!: string;
+  client!: string;
+  adresse!: string;
+  compose!: string;
+  //description!: string;
+ // hasEquipment!: boolean;
+ // equipements!: string;
+
+  constructor() {
+    /*this.idAgent = 0;
+    this.idClient = 0;
+    this.idProprietaire = 0;
+    this.idProperty = new Property();*/
+    this.dateDebut = new Date;
+    this.dateFin ;
+    this.createdDate;
+    this.typeContrat = 0;
+    this.montant = 0;
+    this.proprietaire="Douaa ZAOUI";
+    this.client="Aziz NAFFETI";
+    this.adresse="Tunis Arianna";
+    this.compose="2 Chambres";
+
+   /* this.hasEquipment = false;
+    this.equipements = "";
+    this.description = "";*/
+  }
+}
+ enum TypeContrat{
+  Default , Appartement , House , Terrain
+}
