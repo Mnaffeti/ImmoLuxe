bf88bc63e35bb5c9fc69521ee2b63b84af9e91fe
Douaa-Zaoui <zaouidoa9@gmail.com>
2024-06-30 13:42:16 +0200

    Delete ImmoLuxe directory

--- ImmoLuxe/server/src/main/java/com/example/immoluxe/Repository/ContratRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.immoluxe.Repository;
-
-import com.example.immoluxe.Entity.Contrat;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface ContratRepository extends JpaRepository<Contrat,Long>{
-}
