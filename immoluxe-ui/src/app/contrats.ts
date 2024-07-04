d272010ace2f74c83190856e1e1d4eb63a320605
Douaa ZAOUI <zaouidoa9@gmail.com>
2024-07-04 08:09:51 +0200

    update contrat html

--- immoluxe-ui/src/app/contrats.ts
+++ immoluxe-ui/src/app/contrats.ts
@@ -17,9 +17,18 @@ export class Contrat {
   adresse!: string;
   compose!: string;
   but!: string;
-  //description!: string;
-  // hasEquipment!: boolean;
-  // equipements!: string;
+  description!: string;
+  venteParagraph1!:boolean;
+  venteParagraph2!:boolean;
+  venteParagraph3!:boolean;
+  locationParagraph1!:boolean;
+  locationParagraph2!:boolean;
+  locationParagraph3!:boolean;
+
+  generalParagraph1!:boolean;
+  generalParagraph2!:boolean;
+  generalParagraph3!:boolean;
+  generalParagraph4!:boolean;
 
   constructor() {
     /*this.idAgent = 0;
@@ -29,23 +38,34 @@ export class Contrat {
     this.dateDebut =  new Date();
     this.dateFin  =  new Date();
     this.createdDate =  new Date();
-    this.typeContrat = TypeContrat.Default;
+    this.typeContrat = TypeContrat.None;
     this.montant = 0;
     this.proprietaire="Douaa ZAOUI";
     this.client="Aziz NAFFETI";
     this.adresse="Tunis Arianna";
     this.compose="2 Chambres";
     this.but="Inconnu";
+    this.description = "";
+    this.venteParagraph1=false;
+    this.venteParagraph2=false;
+    this.venteParagraph3=false;
+    this.locationParagraph1=false;
+    this.locationParagraph2=false;
+    this.locationParagraph3=false;
 
+    this.generalParagraph1=false;
+    this.generalParagraph2=false;
+    this.generalParagraph3=false;
+    this.generalParagraph4=false;
     /*this.hasEquipment = false;
     this.equipements = "";
-    this.description = "";*/
+    */
   }
 }
 
-enum TypeContrat{
-  Default,
-  Appartement,
-  House,
-  Terrain
+export enum TypeContrat {
+  None = '',
+  Vente = 'Vente',
+  Location = 'Location',
+  Construction = 'Construction'
 }
